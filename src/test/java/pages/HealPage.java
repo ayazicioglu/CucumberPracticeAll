46e17d15cbcc24740649622ba75c55cbe794a8ad
ayazicioglu <aahmetyazicioglu@gmail.com>
2023-05-10 17:10:17 +0300

    10.5.23

---
+++ src/test/java/pages/HealPage.java
@@ -0,0 +1,13 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HealPage {
+
+    @FindBy (xpath = "//*[text()='OPD']")
+    public WebElement OPDmodule;
+
+
+
+}
